dd536bc054c43584ca3a94cc5f5c386783229a61
Sean Anderson <seanpanderson49@gmail.com>
2019-04-19 17:33:26 -0500

    Edits to Dashboard

---
+++ Dashboard/dashboard.php
@@ -0,0 +1,6 @@
+<main>
+    <a href="../Account"><div class="dash_button"><span class="dash_button_text">Configure Account</span></div></a>
+    <a href="../AccessGroups"><div class="dash_button"><span class="dash_button_text">Configure Access Groups</span></div></a>
+    <a href="../Room"><div class="dash_button"><span class="dash_button_text">Start Classroom</span></div></a>
+    <a href="../Reports"><div class="dash_button"><span class="dash_button_text">Quiz Reports</span></div></a>
+</main>
