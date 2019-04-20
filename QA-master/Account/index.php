63c2c376d89d634fe311ca1e587e754efac8054e
Matterom <Geekey265@gmail.com>
2019-04-20 01:04:47 -0500

    i see what he meant

--- QA-master/Account/index.php
+++
@@ -1,5 +0,0 @@
-<?php include 'function-modules/view/header.php';?>
-<!--This is where successful login attempts are sent, This is the configuration hub for  rooms owned by the user and the account-->
-<main>
-</main>
-<?php include 'function-modules/view/footer.php';?>
--- QA-master/model/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define("DATABASE", "qaProject");
-    define("HOST", "localhost");
-    define("USER", "quesys");
-    define("PASSWORD", "SyqTSnV99jDuDn4z");
-    //Toggles
-    define("SECURE", FALSE)
-?>
