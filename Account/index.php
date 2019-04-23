e2cbedac8feae884f67c3abe5a22fc1c012c5efb
Nayeli <garcia.n93@gmail.com>
2019-04-23 10:49:15 -0500

    Create .DS_Store

--- Account/index.php
+++
@@ -1,5 +0,0 @@
-<?php include '../view/header.php';?>
-<!--This is where successful login attempts are sent, This is the configuration hub for  rooms owned by the user and the account-->
-<main>
-</main>
-<?php include '../view/footer.php';?>
