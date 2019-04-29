705535603b6b91791ac4272826837dcc6e339eea
Nayeli <garcia.n93@gmail.com>
2019-04-29 10:44:30 -0500

    Update index.php

--- Room/index.php
+++ Room/index.php
@@ -4,15 +4,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <main>
-<div id="QuestionBox" class="hidden">
+<div id="QuestionBox">
   <div id="QuestionHeader">
       <p id="QText" class="Question">Question</p>
       <p id="Time" class="Timer"><p>
   </div>
   <div id="AnswerGrid">
-    <button onclick="updateQuiz('User', 1)" id="A5" class="Answer">Answer</button>
-    <button onclick="updateQuiz('User', 2)" id="A5" class="Answer">Answer</button>
-    <button onclick="updateQuiz('User', 3)" id="A5" class="Answer">Answer</button>
+    <button onclick="updateQuiz('User', 1)" id="A1" class="Answer">Answer</button>
+    <button onclick="updateQuiz('User', 2)" id="A2" class="Answer">Answer</button>
+    <button onclick="updateQuiz('User', 3)" id="A3" class="Answer">Answer</button>
     <button onclick="updateQuiz('User', 4)" id="A4" class="Answer">Answer</button>
     <button onclick="updateQuiz('User', 5)" id="A5" class="Answer">Answer</button>
   </div>
