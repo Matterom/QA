dfcce096b16d9185acadc942d4b0a3f8de83cf03
Nayeli <garcia.n93@gmail.com>
2019-04-29 10:41:45 -0500

    Update index.php

--- Room/index.php
+++ Room/index.php
@@ -1,18 +1,20 @@
 <?php include '../view/header.php';?>
 <!--This handles the user page for rooms EG A drops them here -->
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <main>
 <div id="QuestionBox" class="hidden">
   <div id="QuestionHeader">
-      <p id="QText" class="Question">Placeholder
-      </p>
+      <p id="QText" class="Question">Question</p>
       <p id="Time" class="Timer"><p>
   </div>
   <div id="AnswerGrid">
-    <button onclick="updateQuiz('User', 1)" id="A5" class="Answer">Placeholder</button>
-    <button onclick="updateQuiz('User', 2)" id="A5" class="Answer">Placeholder</button>
-    <button onclick="updateQuiz('User', 3)" id="A5" class="Answer">Placeholder</button>
-    <button onclick="updateQuiz('User', 4)" id="A4" class="Answer">Placeholder</button>
-    <button onclick="updateQuiz('User', 5)" id="A5" class="Answer">Placeholder</button>
+    <button onclick="updateQuiz('User', 1)" id="A5" class="Answer">Answer</button>
+    <button onclick="updateQuiz('User', 2)" id="A5" class="Answer">Answer</button>
+    <button onclick="updateQuiz('User', 3)" id="A5" class="Answer">Answer</button>
+    <button onclick="updateQuiz('User', 4)" id="A4" class="Answer">Answer</button>
+    <button onclick="updateQuiz('User', 5)" id="A5" class="Answer">Answer</button>
   </div>
 </div>
 </main>
